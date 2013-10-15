f32eec016c17c52bf7a7e7fa9b10bf2054567231
David Gwartney <dgwartney@zynga.com>
2013-10-14 20:30:49 -0700

    done

--- computing/assignment3/rankall.R
+++ computing/assignment3/rankall.R
@@ -1,5 +1,16 @@
+
+
+
+
 rankall <- function(outcome, num = "best") {
-  ## Read outcome data
+  results <- data.frame(hospital=character(0),state=character(0))
+  states <- read.csv("states.csv",stringsAsFactors=FALSE)
+  ## For each state, find the hospital of the given rank
+  for (state in states$Abbreviation) {
+    hospital <- rankhospital(state,outcome,num)
+    r <- data.frame(hospital,state)
+    results <- rbind(results,r)
+  }
+  #rownames(results) <- states
+  return(results)
 }
-## Check that state and outcome are valid
-## For each state, find the hospital of the given rank
