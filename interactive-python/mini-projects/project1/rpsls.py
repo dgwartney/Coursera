0e02962ce8adca0334577fb0bc636c5b1c8d41de
David Gwartney <david.gwartney@gmail.com>
2013-10-19 16:09:03 -0700

    Remove unit tests out since code sculptor does not have unittest module

--- interactive-python/mini-projects/project1/rpsls.py
+++ interactive-python/mini-projects/project1/rpsls.py
@@ -1,11 +1,8 @@
 # Rock-paper-scissors-lizard-Spock template
 
-# Unit testing framework
-import unittest
+
 # Random number generation framework
 import random
-# Standard system calls
-import sys
 
 # The key idea of this program is to equate the strings
 # "rock", "paper", "scissors", "lizard", "Spock" to numbers
@@ -157,109 +154,8 @@ def rpsls(name):
     print('Computer chooses ' + computer_name)
     print(outcome)
 
-#
-# Unit Tests
-#
-class TestRPSLS(unittest.TestCase):
-    
-    def setUp(self):
-        """ (self) -> None
-
-        Setup scaffolding for the tests
-        """
-        # Set a seed so our results are repeatable
-        random.seed(1)
-
-#
-# Create our test cases using this mapping:
-# 0 - rock
-# 1 - Spock
-# 2 - paper
-# 3 - lizard
-# 4 - scissors
-
-#
-# Test cases for number_to_name()
-#
-    def test_number_to_name_0(self):
-        self.assertEqual(number_to_name(0),'rock')
-        
-    def test_number_to_name_1(self):
-        self.assertEqual(number_to_name(1),'Spock')
-        
-    def test_number_to_name_2(self):
-        self.assertEqual(number_to_name(2),'paper')
-        
-    def test_number_to_name_3(self):
-        self.assertEqual(number_to_name(3),'lizard')
-        
-    def test_number_to_name_4(self):
-        self.assertEqual(number_to_name(4),'scissors')
-
-    def test_number_to_name_100(self):
-        self.assertEqual(number_to_name(100),'unknown')
-        
-    def test_number_to_name_negative1(self):
-        self.assertEqual(number_to_name(-1),'unknown')
-
-
-#
-# Test cases for name_to_number()
-#
-    def test_name_to_number_rock(self):
-        self.assertEqual(name_to_number('rock'),0)
-
-    def test_name_to_number_Spock(self):
-        self.assertEqual(name_to_number('Spock'),1)
-
-    def test_name_to_number_paper(self):
-        self.assertEqual(name_to_number('paper'),2)
-
-    def test_name_to_number_lizard(self):
-        self.assertEqual(name_to_number('lizard'),3)
-
-    def test_name_to_number_scissors(self):
-        self.assertEqual(name_to_number('scissors'),4)
-
-    def test_name_to_number_thumb(self):
-        self.assertEqual(name_to_number('thumb'),9999)
-        
-    def test_name_to_number_hand(self):
-        self.assertEqual(name_to_number('hand'),9999)
-
-
-
-# Computer numbers when seed is set to 1: 3, 0, 1, 3, 0
-
-#
-# Requires that we redirect standard out, unable to find a quick solution so manually test
-#
-##    def test_rpsls_rock(self):
-##        result = "Player chooses rock\nComputer chooses lizard\nComputer wins1"
-##        self.assertEqual(rpsls("rock"),result)
-##        
-##    def test_rpsls_rock(self):
-##        result = "Player chooses Spock\nComputer chooses rock\nComputer wins!"
-##        self.assertEqual(rpsls("rock"),result)
-        
-
-
-
-
-def run_unit_tests():
-    """ (None) -> None
-
-    Runs the unit tests to validate code
-    """
-    unittest.main()
-#
-# Run our unit tests
-#
-## run_unit_tests()
-
 # Set seed before running so the sequence repeats for testing
 ##random.seed(1)
-
 
 # test your code
 rpsls("rock")
