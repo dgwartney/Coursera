863ca27cf348ee489ea3134a8f258effd0611ca3
David Gwartney <david.gwartney@gmail.com>
2013-10-21 19:32:02 -0700

    Passed tests

--- computing/assignment4/count.R
+++ computing/assignment4/count.R
@@ -1,7 +1,33 @@
 count <- function(cause = NULL) {
   ## Check that "cause" is non-NULL; else throw error
+  if (is.null(cause)) {
+    stop()
+  }
+  
   ## Check that specific "cause" is allowed; else throw error
+  acceptable_causes = c('asphyxiation','blunt force','other','shooting', 'stabbing','unknown')
+  if (any(acceptable_causes == cause))
   ## Read "homicides.txt" data file
+  homicides <- readLines('homicides.txt')
   ## Extract causes of death
+  homicide_count <- 0
+  if (cause == 'asphyxiation') {
+    homicide_count <- length(grep('Cause: [Aa]sphyxiation',homicides))
+  } else if (cause == 'blunt force') {
+    
+  } else if (cause == 'other') {
+    
+  } else if (cause == 'shooting') {
+    homicide_count <- length(grep('Cause: [Ss]hooting',homicides))
+  } else if (cause == 'stabbing') {
+    homicide_count <- length(grep('Cause: [Ss]tabbing',homicides))
+  } else if (cause == 'unknown') {
+    
+  }
+  else {
+    stop()
+  }
+
   ## Return integer containing count of homicides for that cause
+  return(homicide_count)
 }
